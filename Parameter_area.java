142bd5b68879877ddc7c5044fbb97562469f7c52
NEHAKARDAM2000 <132893648+NEHAKARDAM2000@users.noreply.github.com>
2023-10-26 14:55:57 +0530

    Add files via upload

---
+++ Parameter_area.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Parameter_area {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the length of rectangle");
+        int l=sc.nextInt();
+        System.out.println("enter the breadth of rectangle");
+        int b=sc.nextInt();
+        
+
+        
+    }
+    
+}
